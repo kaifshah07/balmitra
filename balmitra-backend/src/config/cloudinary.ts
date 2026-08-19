c015449cd80c0a2a8e4bfd59318561ba2808ad93
Kaif Shah <shahkaif004@gmail.com>
2026-08-19 19:07:06 +0530

    temporary change , cloudnary.ts file

--- balmitra-backend/src/config/cloudinary.ts
+++ balmitra-backend/src/config/cloudinary.ts
@@ -1,4 +1,20 @@
+// import { v2 as cloudinary } from "cloudinary";
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// export default cloudinary;
+
 import { v2 as cloudinary } from "cloudinary";
+
+console.log("CLOUDINARY CHECK:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key_exists: !!process.env.CLOUDINARY_API_KEY,
+  api_secret_exists: !!process.env.CLOUDINARY_API_SECRET,
+});
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
